29c7e539d1c8cbdf1796e3be9ee875ca1d1947fb
pablal levy <pablo.levy@epitech.eu>
2024-01-07 16:21:02 +0100

    feat: now get the right date

--- src/pages/EventsTable.tsx
+++ src/pages/EventsTable.tsx
@@ -1,42 +1,89 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import './EventsTable.css';
+
+interface Event {
+    date_start: string;
+    date_end: string;
+    title: string;
+    price_detail: string | null;
+    price_type: string;
+}
 
 const EventsTable: React.FC = () => {
-    const [events, setEvents] = useState(null);
-    const [startDate, setStartDate] = useState<string | null>(null);
-    const [endDate, setEndDate] = useState<string | null>(null);
+    const [events, setEvents] = useState<any[]>([]); 
     const navigate = useNavigate();
 
-
     useEffect(() => {
-        const startDate = localStorage.getItem('startDate');
-        const endDate = localStorage.getItem('endDate');
-        if (startDate && endDate) {
+        getEvents().then()
+    }, []);
 
-            setStartDate(startDate);
-            setEndDate(endDate);
+    const getEvents = async () => {
+        const storedStartDate = localStorage.getItem('startDate');
+        const storedEndDate = localStorage.getItem('endDate');
+
+        if (storedStartDate && storedEndDate) {
+            try {
+                const response = await axios.get('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=70');
+
+                const filteredEvents = response.data.results.filter((event: any) => {
+                    const eventStartDate = new Date(event.date_start);
+                    const eventEndDate = new Date(event.date_end);
+                    const selectedStartDate = new Date(storedStartDate);
+                    const selectedEndDate = new Date(storedEndDate);
+
+                    return eventStartDate >= selectedStartDate && eventEndDate <= selectedEndDate;
+                });
+
+                setEvents(filteredEvents);
+            } catch (error) {
+                console.error('Error fetching events:', error);
+            }
         } else {
             navigate('/');
         }
-        callAPI().then()
-    }, []);
-    const callAPI = async () => {
-        try {
-            const response = await axios.get('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=20');
-            console.log(response.data);
-            setEvents(response.data.results)
-        } catch (e) {
-            console.log(e)
+
+    }
+    const parseDate = (dateString: string) => {
+        const date = new Date(dateString);
+        return `${date.toLocaleDateString()} à ${date.toLocaleTimeString()}`;
+    }
+
+    function parsePrice(priceString: string | null) {
+        if (!priceString) {
+            return '';
         }
+        const match = priceString.match(/Prix\s*:\s*(\d+)/);
+        if (match) {
+            return `Prix: ${match[1]}€`;
+        }
+        return '';
     }
 
 
-
     return (
-        <table>
-
+        <div>
+        <button onClick={() => navigate('/')}>Retour</button>
+        <table className="events-table">
+            <thead>
+            <tr>
+                <th>Date de Début</th>
+                <th>Titre</th>
+                <th>Tarif</th>
+            </tr>
+            </thead>
+            <tbody>
+            {events.map((event, index) => (
+                <tr key={index}>
+                    <td>{parseDate(event.date_start) || ''}</td>
+                    <td>{event.title || ''}</td>
+                    <td>{event.price_type === 'gratuit' ? 'Gratuit' : parsePrice(event.price_detail) || 'non spécifié'}</td>
+                </tr>
+            ))}
+            </tbody>
         </table>
+        </div>
     );
 };
 
